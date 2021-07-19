9f44837112ee39d3ee9129f2d2eeeeee4ae3e3c6
Ivo Leonardo SC <developer.ivo.leonardo@gmail.com>
2021-07-19 16:24:06 -0400

    Primeiro commit do projeto.

---
+++ src/test/java/br/com/ilsc/aluraflix/AluraflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ilsc.aluraflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
